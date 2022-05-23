a5765e9ee56ad35aea01dc57cf6d2a4b8924f610
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-22 23:11:12 -0700

    navbar and route set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
+    <div >
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
+      </Routes>
     </div>
   );
 }
