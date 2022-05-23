763bdf4316e9dc5e59a12cf6d8ac7ccb9d96aa7a
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-22 22:11:31 -0700

    theme change and mofidications

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        tools: {
+          primary: "#570DF8",
+          secondary: "#f6d860",
+          accent: "#37CDBE",
+          neutral: "#3d4451",
+          info: "#3ABFF8",
+          warning: "#FBBD23",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
