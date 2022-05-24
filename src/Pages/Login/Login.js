8df2b11cde4df1e3188bd58dfdbde5f62994a174
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-24 12:55:44 -0700

    google login set up

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='flex justify-center items-center h-screen'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl font-bold">Login</h2>
+                    <div class="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline btn-primary">Continue with google</button>
+                </div>
+            </div>
         </div>
     );
 };
